963766d2f671f67e41c09e97a0a144d4d9393c21
CJM <cjm53415341@gmail.com>
2020-10-01 23:37:08 +0900

    upload

---
+++ app/src/main/java/com/example/punchpower/ResultActivity.kt
@@ -0,0 +1,25 @@
+package com.example.punchpower
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_result.*
+
+class ResultActivity : AppCompatActivity() {
+
+    val power by lazy{
+        intent.getDoubleExtra("power", 0.0) *100
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        title = "펀치력결과"
+
+        scoreLabel.text = "${String.format("%.0f", power)} 점"
+
+        restartButton.setOnClickListener{
+            finish()
+        }
+    }
+}
